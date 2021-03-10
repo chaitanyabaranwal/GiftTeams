c9d57c1bfae997d0582a15b89fbb6969bc22c28b
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2021-03-10 23:45:30 +0800

    Add signup page

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,5 +20,6 @@ from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
