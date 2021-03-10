49ba7261a0fb9eb9b309c72f0abc0b5da682a98e
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2021-03-10 23:56:13 +0800

    Add pycache to gitignore

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 class HRPerson(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
